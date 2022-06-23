d5586a5ec70b1ded3bce93841195b5af342f8325
Yuri <aoyamabrasil@gmail.com>
2022-06-23 12:38:05 -0300

    preparando BD

---
+++ django/receitas/migrations/0001_initial.py
@@ -0,0 +1,26 @@
+import datetime
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Receita',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('nome_receita', models.CharField(max_length=50)),
+                ('ingredientes', models.TextField()),
+                ('modo_preparo', models.TextField()),
+                ('tempo_preparo', models.IntegerField()),
+                ('rendimento', models.CharField(max_length=20)),
+                ('categoria', models.CharField(max_length=20)),
+                ('data_receita', models.DateTimeField(blank=True, default=datetime.datetime.now)),
+            ],
+        ),
+    ]
--- django/receitas/models.py
+++ django/receitas/models.py
@@ -1,3 +1,12 @@
+from datetime import datetime
 from django.db import models
 
-# Create your models here.
+
+class Receita(models.Model):
+    nome_receita = models.CharField(max_length=50)
+    ingredientes = models.TextField()
+    modo_preparo = models.TextField()
+    tempo_preparo = models.IntegerField()
+    rendimento = models.CharField(max_length=20)
+    categoria = models.CharField(max_length=20)
+    data_receita = models.DateTimeField(default=datetime.now, blank=True)
