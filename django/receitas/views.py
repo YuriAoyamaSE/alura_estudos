d033f79e9498f5c3135f43088de1d0e2b31638b8
Yuri <aoyamabrasil@gmail.com>
2022-06-25 17:05:21 -0300

    terminando curso

--- django/receitas/views.py
+++ django/receitas/views.py
@@ -1,22 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import Receita
+
+# def index(request):
+#     receitas = {
+#         1: 'Lasanha',
+#         2: 'Sopa de legumes',
+#         3: 'Sorvete',
+#         4: 'Bolo de chocolate',
+#         5: 'Pudim',
+#         6: 'Pavê',
+#     }
+#     dados = {
+#         'dict_receitas': receitas
+#     }
+#     return render(request, 'index.html', dados)
 
 
 def index(request):
-    receitas = {
-        1: 'Lasanha',
-        2: 'Sopa de legumes',
-        3: 'Sorvete',
-        4: 'Bolo de chocolate',
-        5: 'Pudim',
-        6: 'Pavê',
-    }
-
-    dados = {
-        'dict_receitas': receitas
-    }
-
+    receitas = Receita.objects.all()
+    dados = {'receitas': receitas}
     return render(request, 'index.html', dados)
 
 
-def receita(request):
-    return render(request, 'receita.html')
+def receita(request, receita_id):
+    receita = get_object_or_404(Receita, pk=receita_id)
+    receita_a_exibir = {'receita' : receita}
+    return render(request, 'receita.html', receita_a_exibir)
