8d0cdc353d96e56b127e227392c6372aff698f51
Yuri <aoyamabrasil@gmail.com>
2022-06-25 15:24:45 -0300

    resolvendo .env

--- django/alurareceita/settings.py
+++ django/alurareceita/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,11 +81,11 @@ WSGI_APPLICATION = 'alurareceita.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'alura_receitas',
-        'USER': 'root',
-        'PASSWORD': 'TaigaSQL22',
-        'HOST': '127.0.0.1',
-        'PORT': '3306'
+        'NAME': os.getenv('NAME'),
+        'USER': os.getenv('USER'),
+        'PASSWORD': os.getenv('PASSWORD'),
+        'HOST': os.getenv('HOST'),
+        'PORT': os.getenv('PORT')
     }
 }
 
