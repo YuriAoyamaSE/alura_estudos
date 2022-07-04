23451ca05cc1b73f933ecf570071c588b7fc6a2b
Yuri <aoyamabrasil@gmail.com>
2022-07-04 12:42:50 -0300

    salvando projeto atualizado

--- django/pessoas/models.py
+++ django/pessoas/models.py
@@ -3,7 +3,6 @@ from django.db import models
 class Pessoa(models.Model):
     nome = models.CharField(max_length=200)
     email = models.CharField(max_length=200)
-    
-    
-    def __str__(self) -> str:
+    def __str__(self):
         return self.nome
+
--- django/receitas/models.py
+++ django/receitas/models.py
@@ -1,21 +1,17 @@
-from datetime import datetime
-from distutils.command.upload import upload
-from email.policy import default
 from django.db import models
+from datetime import datetime
 from pessoas.models import Pessoa
 
 class Receita(models.Model):
     pessoa = models.ForeignKey(Pessoa, on_delete=models.CASCADE)
-    nome_receita = models.CharField(max_length=50)
+    nome_receita = models.CharField(max_length=200)
     ingredientes = models.TextField()
     modo_preparo = models.TextField()
     tempo_preparo = models.IntegerField()
-    rendimento = models.CharField(max_length=20)
-    categoria = models.CharField(max_length=20)
-    data_receita = models.DateTimeField(default=datetime.now, blank=True)
-    foto_receita = models.ImageField(upload_to='fotos-receitas/%d/%m/%Y/', blank=True)
+    rendimento = models.CharField(max_length=100)
+    categoria = models.CharField(max_length=100)
+    date_receita = models.DateTimeField(default=datetime.now, blank=True)
+    foto_receita = models.ImageField(upload_to='fotos/%d/%m/%Y/', blank=True)
     publicada = models.BooleanField(default=False)
-
-    def __str__(self) -> str:
+    def __str__(self):
         return self.nome_receita
-
