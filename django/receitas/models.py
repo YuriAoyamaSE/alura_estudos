4412a611ee32eb4c05dfcd2bd056cae300d8d551
Yuri <aoyamabrasil@gmail.com>
2022-07-03 22:37:39 -0300

    incrementando o projeto

--- django/receitas/models.py
+++ django/receitas/models.py
@@ -1,8 +1,9 @@
 from datetime import datetime
 from django.db import models
-
+from pessoas.models import Pessoa
 
 class Receita(models.Model):
+    pessoa = models.ForeignKey(Pessoa, on_delete=models.CASCADE)
     nome_receita = models.CharField(max_length=50)
     ingredientes = models.TextField()
     modo_preparo = models.TextField()
@@ -10,3 +11,6 @@ class Receita(models.Model):
     rendimento = models.CharField(max_length=20)
     categoria = models.CharField(max_length=20)
     data_receita = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self) -> str:
+        return self.nome_receita
