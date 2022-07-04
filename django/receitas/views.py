b1fd52b683f19aafe327f0f5c06263d9a92ccf5d
Yuri <aoyamabrasil@gmail.com>
2022-07-04 12:24:03 -0300

    termino parcial do curso

--- django/receitas/views.py
+++ django/receitas/views.py
@@ -11,3 +11,17 @@ def receita(request, receita_id):
     receita = get_object_or_404(Receita, pk=receita_id)
     receita_a_exibir = {'receita' : receita}
     return render(request, 'receita.html', receita_a_exibir)
+
+def buscar(request):
+    lista_receitas = Receita.objects.order_by('-date_receita').filter(publicada=True)
+
+    if nome_a_buscar in request.GET:
+        nome_a_buscar = request.GET['buscar']
+        if buscar:
+            lista_receitas = lista_receitas.filter(nome_receita__icontains=nome_a_buscar)
+
+    dados = {
+        'receitas' : lista_receitas
+    }
+
+    return render(request, 'buscar.html', dados)
